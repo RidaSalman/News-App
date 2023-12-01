efe481b75b8aada90c9a36be35c41fbb100807be
Rida Salman <ridasalman458@gmail.com>
2023-12-01 17:52:49 +0500

    Fourth Commit

---
+++ app/src/main/java/com/example/composeapp/presentation/onboarding/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.composeapp.presentation.onboarding.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class HomeViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/composeapp/presentation/onboarding/home/NewsUseCases.kt
@@ -0,0 +1,8 @@
+package com.example.composeapp.presentation.onboarding.home
+
+import com.example.composeapp.domain.manager.usecases.app_entry.news.GetNews
+
+data class NewsUseCases(
+    val getNews: GetNews,
+    val searchNews: SearchNews
+)
